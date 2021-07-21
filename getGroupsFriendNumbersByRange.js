0f353484355375bef1ab33fc38f94a4c624b4216
Hai-Vasyl <hai.vasyl20@gmail.com>
2021-07-21 16:52:31 +0300

    Added comments to getGroupsFriendNumbersByRange.js file

--- getGroupsFriendNumbersByRange.js
+++ getGroupsFriendNumbersByRange.js
@@ -2,6 +2,11 @@ const checkNaturNumber = require("./helpers/checkNaturNumber.js")
 const calcSumMultiNumbers = require("./helpers/calcSumMultiNumbers.js")
 const isGroupInGroups = require("./helpers/isGroupInGroups.js")
 
+// два натуральних числа називають дружніми,
+// якщо кожне з них дорівнює сумі всіх дільників другого,
+//  крім самого цього числа.
+// Знайти всі пари дружніх чисел, які лежать в діапазоні від 200 до 300
+
 function getGroupsFriendNumbersByRange(from, to) {
   const _from = checkNaturNumber(from)
   const _to = checkNaturNumber(to)
